115c2195609eb057ace51d6ed2e18d335ac2374c
Retno <retnoscwulandari@gmail.com>
2019-10-30 15:20:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import Home from "./Home";
+import History from "./History";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="none">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/our-history" activeClassName="active">
+                History
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home}>
+            <Home />
+          </Route>
+          <Route path="/our-history" component={History}>
+            <History />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
